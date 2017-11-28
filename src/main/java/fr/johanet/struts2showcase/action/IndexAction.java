f2e2f9bc4d24f19b4314c5afe98e6aee7ef16aff
ojathelonius <louis.johanet@epfedu.fr>
2017-11-28 23:54:55 +0100

    Testing struts2 without tags, bootstrap4 only

--- src/main/java/fr/johanet/struts2showcase/action/IndexAction.java
+++ src/main/java/fr/johanet/struts2showcase/action/IndexAction.java
@@ -2,12 +2,30 @@ package fr.johanet.struts2showcase.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import fr.johanet.struts2showcase.model.Product;
+
 public class IndexAction extends ActionSupport {
 
 	private static final long serialVersionUID = 1L;
 
+	private Product product;
+
 	public String execute() throws Exception {
+
 		return SUCCESS;
+	}
+
+	public String add() throws Exception {
+		String toast = product.getName();
+		return SUCCESS;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
 	}
 
 }
---
+++ src/main/java/fr/johanet/struts2showcase/model/Product.java
@@ -0,0 +1,23 @@
+package fr.johanet.struts2showcase.model;
+
+public class Product {
+	private String name;
+	private String comment;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+}
